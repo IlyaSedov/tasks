e638c4b32407c6e9aeb39f916614ad78a03d9b8f
IlyaSedov <55514389+IlyaSedov@users.noreply.github.com>
2019-10-02 14:08:56 +0300

    Update apply.js
    Очень много изменений

--- apply.js
+++ apply.js
@@ -1,8 +1,17 @@
-const apply = (count, fun, val) => {
-    if (count === 0){
-        return fun(val);
-    }
-    
-    return apply(count-1, fun, fun(val))
-}
+const apply = (count = 1, fun = console.log(), val = 0) => {
+  const patternOne = /[^0-9]/,
+    typCountStr = typeof count == 'string',
+    typValStr = typeof val == 'string';
 
+  if ((typCountStr === true) && (count.search(patternOne) == -1)) count = Number(count);
+  if ((typValStr === true) && (val.search(patternOne) == -1)) val = Number(count);
+  if ((typeof count != 'number') || (typeof val != 'number')) throw new TypeError('Данные некорректны');
+
+  if (count === 0) {
+    return val;
+  }
+
+  return apply(count - 1, fun, fun(val))
+};
+
+export default apply;
