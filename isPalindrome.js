2bc765b8c4b1df9ae905ed8a0d7d35282917d3c3
IlyaSedov <55514389+IlyaSedov@users.noreply.github.com>
2019-10-04 20:59:22 +0300

    adding features
    Добавлена проверка регулярным выражением

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,7 +1,10 @@
 const isPalindrome = (str) => {
   const patternOne = /[^0-9,+-]/;
   if ((typeof str !== 'string') && (typeof str !== 'number')) throw new TypeError('The data is incorrect');
-  str = String(str);
+  
+  str = String(str).toLowerCase(); 
+  str = str.replace(/[^а-яa-z0-9]/g, '');
+  
   if (str.length === 0) throw new Error('empty string');
   if (str.length === 1) return true;
 
@@ -15,9 +18,9 @@ const isPalindrome = (str) => {
 
     return result(count.slice(1, -1));
 
-  }
+  };
   return result(str);
-}
+};
 
 
 export default isPalindrome();
