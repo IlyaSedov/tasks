5c7cbb1af272d210685f9bbeb11e8c0d3320ea4e
IlyaSedov <55514389+IlyaSedov@users.noreply.github.com>
2019-10-04 15:26:36 +0300

    Update apply.js
    Добавлены проверки на получение аргументов и отрицательное кол-во повторений

--- apply.js
+++ apply.js
@@ -1,12 +1,14 @@
-const apply = (count = 1, fun, val = 0) => {
+const apply = (count, fun, val) => {
   const patternOne = /[^0-9]/,
     typCountStr = typeof count == 'string',
     typValStr = typeof val == 'string';
 
-  if((fun == undefined)||(typeof fun!='function'))return console.log ('функция не передана')
+  if (val == undefined) return console.log('одно или несколько значений не переданы')
+  if ((fun == undefined) || (typeof fun != 'function')) return console.log('функция не передана')
   if ((typCountStr === true) && (count.search(patternOne) == -1)) count = Number(count);
   if ((typValStr === true) && (val.search(patternOne) == -1)) val = Number(count);
   if ((typeof count != 'number') || (typeof val != 'number')) throw new TypeError('Данные некорректны');
+  if (cont < 0) count = 0;
 
   if (count === 0) {
     return val;
@@ -14,5 +16,6 @@ const apply = (count = 1, fun, val = 0) => {
 
   return apply(count - 1, fun, fun(val))
 };
+
 
 export default apply;
