5210957fcce7c0bc8f26bf0df582bffb5571aa74
IlyaSedov <55514389+IlyaSedov@users.noreply.github.com>
2019-10-04 19:41:48 +0300

    Update substr.js

--- substr.js
+++ substr.js
@@ -2,7 +2,7 @@ const substr = (str, startIndex = 0, lengthStr = str.length) => {
   let finishSubstr = '';
   
   if (typeof str !== 'string') throw new TypeError('Данные некорректны');
-  if ((str.length === 0)||(startIndex > str.length)||(lengthStr === 0)) return console.log(''); 
+  if ((str.length === 0) || (startIndex > str.length) || (lengthStr === 0)) return console.log(''); 
   if (lengthStr < 0) lengthStr = 1;
   if (startIndex < 0) startIndex = 0;
   if ((startIndex + lengthStr - 1) > str.length) lengthStr = str.length - startIndex;
@@ -11,7 +11,7 @@ const substr = (str, startIndex = 0, lengthStr = str.length) => {
     finishSubstr += str[i];
   }
 
-  return console.log(finishSubstr);
+  return finishSubstr;
 };
 
 export default substr;
