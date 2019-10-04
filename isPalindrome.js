988f31454ad9a77ce7585834521b0d952e4b0405
IlyaSedov <55514389+IlyaSedov@users.noreply.github.com>
2019-10-04 19:49:04 +0300

    Update isPalindrome.js

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,22 +1,23 @@
 const isPalindrome = (str) => {
-  if ((typeof str !== 'string') && (typeof str !== 'number')) throw new TypeError('Данные некорректны');
-  
+  const patternOne = /[^0-9,+-]/;
+  if ((typeof str !== 'string') && (typeof str !== 'number')) throw new TypeError('The data is incorrect');
   str = String(str);
-  
-  if (str.length === 0) return 'Пустая строка';
-  if (str.length == 1) return true;
+  if (str.length === 0) throw new TypeError('empty string');
+  if (str.length === 1) return true;
+
   const result = (count) => {
     if (count.length < 2) {
       return true;
     }
-    if (count[0] !== count[count.length - 1]) {
+    else if (count[0] !== count[count.length - 1]) {
       return false;
     }
 
     return result(count.slice(1, -1));
-  };
-  
+
+  }
   return result(str);
-};
+}
+
 
 export default isPalindrome();
