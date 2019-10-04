54412d68f65a89861fb865398f83e2eead4d41dd
IlyaSedov <55514389+IlyaSedov@users.noreply.github.com>
2019-10-04 19:39:00 +0300

    Update apply.js

--- apply.js
+++ apply.js
@@ -1,14 +1,14 @@
 const apply = (count, fun, val) => {
   const patternOne = /^\d*$/,
-    typCountStr = typeof count === 'string',
-    typValStr = typeof val === 'string';
-
+    typCountStr = typeof count == 'string',
+    typValStr = typeof val == 'string';
+  
   if (val === undefined) return console.log('одно или несколько значений не переданы')
   if ((fun === undefined) || (typeof fun != 'function')) return console.log('функция не передана')
-  if ((typCountStr === true) && (count.search(patternOne) == -1)) count = Number(count);
-  if ((typValStr === true) && (val.search(patternOne) == -1)) val = Number(count);
+  if ((typCountStr === true) && (count.search(patternOne) >= 0)) count = Number(count);
+  if ((typValStr === true) && (val.search(patternOne) >= 0)) val = Number(count);
   if ((typeof count != 'number') || (typeof val != 'number')) throw new TypeError('Данные некорректны');
-  if (cont < 0) count = 0;
+  if (count < 0) count = 0;
 
   if (count === 0) {
     return val;
