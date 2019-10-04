0c70daab28a5ed589aa1a2bf332fe2195fb81072
IlyaSedov <55514389+IlyaSedov@users.noreply.github.com>
2019-10-04 19:12:25 +0300

    Update apply.js
    Изменение регулярного выражения

--- apply.js
+++ apply.js
@@ -1,10 +1,10 @@
 const apply = (count, fun, val) => {
-  const patternOne = /[^0-9]/,
-    typCountStr = typeof count == 'string',
-    typValStr = typeof val == 'string';
+  const patternOne = /^\d*$/,
+    typCountStr = typeof count === 'string',
+    typValStr = typeof val === 'string';
 
-  if (val == undefined) return console.log('одно или несколько значений не переданы')
-  if ((fun == undefined) || (typeof fun != 'function')) return console.log('функция не передана')
+  if (val === undefined) return console.log('одно или несколько значений не переданы')
+  if ((fun === undefined) || (typeof fun != 'function')) return console.log('функция не передана')
   if ((typCountStr === true) && (count.search(patternOne) == -1)) count = Number(count);
   if ((typValStr === true) && (val.search(patternOne) == -1)) val = Number(count);
   if ((typeof count != 'number') || (typeof val != 'number')) throw new TypeError('Данные некорректны');
