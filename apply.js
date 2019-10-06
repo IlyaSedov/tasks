7106450a677f6f0a13e9a614adf8f74089940ff1
IlyaSedov <55514389+IlyaSedov@users.noreply.github.com>
2019-10-06 19:05:27 +0300

    Valbag
    Исправил val

--- apply.js
+++ apply.js
@@ -1,13 +1,11 @@
 const apply = (count, fun, val) => {
   const patternOne = /^\d*$/,
     typCountStr = typeof count === 'string',
-    typValStr = typeof val === 'string';
   
   if (val === undefined) return console.log('one or more values are not passed')
   if ((fun === undefined) || (typeof fun != 'function')) return console.log('the function is not passed')
   if ((typCountStr === true) && (count.search(patternOne) >= 0)) count = Number(count);
-  if ((typValStr === true) && (val.search(patternOne) >= 0)) val = Number(val);
-  if ((typeof count != 'number') || (typeof val != 'number')) throw new TypeError('The data is incorrect');
+  if (typeof count != 'number') throw new TypeError('The data is incorrect');
   if (count < 0) count = 0;
 
   if (count === 0) {
